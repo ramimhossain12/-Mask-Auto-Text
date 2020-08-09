4bfbddee4578d645857a7f252a3dd72d2784b8f3
ramimhossain12 <ramimhossain6242@gmail.com>
2020-08-10 00:23:40 +0600

    new library added

--- app/src/main/java/com/example/maskedittext/MainActivity.java
+++ app/src/main/java/com/example/maskedittext/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.maskedittext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.santalu.maskara.widget.MaskEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MaskEditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void showText(View v) {
+
+    }
+    public void showRawText(View v) {
+
     }
 }
